c3435f0822e2f764725296015a619838d1a3482e
ViniciusDSLima <vinicius.devjvm@gmail.com>
2025-06-13 17:30:41 -0300

    feat: add new props on travel entity and add update travel request method

--- internal/domain/entity/travel.go
+++ internal/domain/entity/travel.go
@@ -2,8 +2,9 @@ package entity
 
 import (
 	"challenge-travel-api/internal/domain/enums"
-	"github.com/google/uuid"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type TravelRequest struct {
@@ -14,8 +15,58 @@ type TravelRequest struct {
 	DepartureDate   time.Time                 `json:"departure_date" gorm:"type:timestamp;not null"`
 	ReturnDate      *time.Time                `json:"return_date" gorm:"type:timestamp;"`
 	Status          enums.TravelRequestStatus `json:"status" gorm:"type:travel_request_status;not null"`
+	CanceledBy      *uuid.UUID                `json:"canceled_by" gorm:"type:uuid"`
+	ApprovedBy      *uuid.UUID                `json:"approved_by" gorm:"type:uuid"`
 	CreatedAt       time.Time                 `json:"created_at" gorm:"type:timestamp;not null"`
 	UpdatedAt       *time.Time                `json:"updated_at" gorm:"type:timestamp"`
+	CanceledAt      *time.Time                `json:"canceled_at" gorm:"type:timestamp"`
+	ApprovedAt      *time.Time                `json:"approved_at" gorm:"type:timestamp"`
 
 	User User `json:"user" gorm:"foreignkey:user_id"`
+}
+
+func (e *TravelRequest) UpdateTravelRequest(
+	destinationName,
+	travelerName *string,
+	departureDate,
+	returnDate *time.Time,
+	status *enums.TravelRequestStatus,
+	canceledBy,
+	approvedBy *uuid.UUID) {
+	if destinationName != nil {
+		e.DestinationName = *destinationName
+	}
+
+	if travelerName != nil {
+		e.TravelerName = *travelerName
+	}
+
+	if departureDate != nil {
+		e.DepartureDate = *departureDate
+	}
+
+	if returnDate != nil {
+		e.ReturnDate = returnDate
+	}
+
+	if status != nil {
+		e.Status = *status
+	}
+
+	if canceledBy != nil {
+		e.CanceledBy = canceledBy
+
+		canceledAt := time.Now()
+		e.CanceledAt = &canceledAt
+	}
+
+	if approvedBy != nil {
+		e.ApprovedBy = approvedBy
+
+		approvedAt := time.Now()
+		e.ApprovedAt = &approvedAt
+	}
+
+	updatedDate := time.Now()
+	e.UpdatedAt = &updatedDate
 }
